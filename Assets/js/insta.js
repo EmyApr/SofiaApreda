e2f73c6936b7596fee40ddd5ec3e4d7bc4edbc64
EmyApr <110528040+EmyApr@users.noreply.github.com>
2024-02-21 17:11:51 +0000

    1

--- Assets/js/insta.js
+++ Assets/js/insta.js
@@ -1,5 +1,6 @@
 
+
     var feed = new Instafeed({
-      accessToken: 'your-token'
+      accessToken: 'IGQWRNWkRybzkzT295R0dBVjVDWHotT2lhOHJISkg2cFJ6SFA1UVpBUGUxZATgxcXk5bC0yNzlxeTFocnVvVmpjb3ByT1Bqay1lUnJpWGNmTjFJMU85UjFvVDlKZAUNuNHV2ZAUhuNTZAEcEJKcWtoTU94Sm1GdE5Ra1EZD'
     });
     feed.run();
