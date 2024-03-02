3a750995b7c40e39eb3dd0dff615cf3abbf66aac
Emyapr <emyapreda@hotmail.co.uk>
2024-03-02 00:19:29 +0000

    gallery

--- Assets/js/insta.js
+++
@@ -1,6 +0,0 @@
-
-
-    var feed = new Instafeed({
-      accessToken: 'IGQWRNWkRybzkzT295R0dBVjVDWHotT2lhOHJISkg2cFJ6SFA1UVpBUGUxZATgxcXk5bC0yNzlxeTFocnVvVmpjb3ByT1Bqay1lUnJpWGNmTjFJMU85UjFvVDlKZAUNuNHV2ZAUhuNTZAEcEJKcWtoTU94Sm1GdE5Ra1EZD'
-    });
-    feed.run();
